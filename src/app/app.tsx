0c6afaadee2803ebd65fb1f2dcfea4dad79164da
nsk0826 <nsk.20160401@gmail.com>
2022-07-06 23:13:31 +0900

    add message

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.FC<{ compiler: string, framework: string }> = (props) => {
+const App: React.FC<{ compiler: string, framework: string ,message:string}> = (props) => {
   return (
     <div>
       <div>hey</div>
       <div>{props.compiler}</div>
       <div>{props.framework}</div>
-      <div>{props.children.toString}</div>
+      <div>{props.message}</div>
     </div>
   );
 }
 
 ReactDOM.render(
-  <App compiler="TypeScript" framework="React" />,
+  <App compiler="TypeScript" framework="React" message="hello"/>,
   document.getElementById("root")
 );
